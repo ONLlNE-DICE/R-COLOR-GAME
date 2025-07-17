d6854cacb1f32015c50f8c0d76077ba1a8ca5d1c
Tarublaks <amborlim28@gmail.com>
2025-07-17 08:37:55 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR.firebaseapp.com",
+  databaseURL: "https://YOUR.firebaseio.com",
+  projectId: "YOUR",
+  storageBucket: "YOUR.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
